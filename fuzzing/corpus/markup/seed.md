455264165a011a076e1409a48857c0af7b925837
VonDoo <ramezwaheeb11@gmail.com>
2026-08-27 01:18:31 +0300

    Delete fuzzing directory

--- fuzzing/corpus/markup/seed.md
+++
@@ -1,8 +0,0 @@
-<!--
-Copyright © Weblate contributors
-
-SPDX-License-Identifier: GPL-3.0-or-later
--->
-
-<b>Hello</b>
-[Weblate](https://weblate.org/)
--- fuzzing/urls.py
+++
@@ -1,6 +0,0 @@
-# Copyright © Weblate contributors
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-from __future__ import annotations
-
-urlpatterns: list = []
