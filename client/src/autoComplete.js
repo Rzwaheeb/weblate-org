42d77a03136bce15891becd05bbcb4f23ab6d887
VonDoo <ramezwaheeb11@gmail.com>
2026-08-27 01:16:48 +0300

    Delete client directory

--- client/src/autoComplete.js
+++
@@ -1,7 +0,0 @@
-// Copyright © Michal Čihař <michal@weblate.org>
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-import autoComplete from "@tarekraafat/autocomplete.js";
-
-window.autoComplete = autoComplete;
--- client/src/bootstrap5.js
+++
@@ -1,8 +0,0 @@
-// Copyright © Michal Čihař <michal@weblate.org>
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-import "bootstrap/dist/css/bootstrap.css";
-import * as bootstrap from "bootstrap";
-
-window.bootstrap = bootstrap;
